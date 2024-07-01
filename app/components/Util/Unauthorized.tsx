ad75e89a8ca3cbee287bb19b3697b807ebd8afe4
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-07-01 14:35:11 +0300

    profile page: profile section, page makeup

---
+++ app/components/Util/Unauthorized.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Unauthorized() {
+  return <div>Unauthorized, please Sign Up or Sign In</div>
+}
--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,8 +1,13 @@
 "use client"
 import { UserAuth } from "@/app/context/AuthContext"
-import { auth, db, doc, getDoc } from "@/app/firebase/config"
+import { db } from "@/app/firebase/config"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import React, { useEffect, useState } from "react"
+import Image from "next/image"
+
+import styles from "./styles.module.css"
+import Loader from "@/app/components/Util/Loader"
+import Unauthorized from "@/app/components/Util/Unauthorized"
 
 export default function Profile() {
   const [userRepsArray, setUserRepsArray]: any = useState(null)
@@ -12,6 +17,8 @@ export default function Profile() {
   useEffect(() => {
     const fetchData = async () => {
       if (user) {
+        console.log(user)
+
         const uid = user.uid
 
         const q = query(collection(db, "usersMaxReps"), where("UID", "==", uid))
@@ -22,8 +29,6 @@ export default function Profile() {
 
         const arrayOfMaxReps = Object.entries(data[0].MaxReps)
 
-        console.log(arrayOfMaxReps)
-
         setUserRepsArray(arrayOfMaxReps)
       }
     }
@@ -32,43 +37,74 @@ export default function Profile() {
   }, [user])
 
   return (
-    <div className="wrapper">
-      <h2>Personal Results: </h2>
-      <ul>
-        {userRepsArray
-          ? userRepsArray.map((sport: any) => {
-              return (
-                <li key={sport[0]}>
-                  <h3>{sport[0]}</h3>
-                  <ul>
-                    {Object.entries(sport[1]).map((sportSubtype: any) => {
-                      console.log(sportSubtype[0])
-
+    <>
+      {user ? (
+        <div className={styles.wrapper}>
+          <div className={styles.header}>
+            <h2 className={styles.heading}>Profile</h2>
+          </div>
+          <div className={styles.body}>
+            <section className={styles.section}>
+              <div className={styles.sectionHeadingWrapper}>
+                <h3 className={styles.sectionHeading}>General information: </h3>
+              </div>
+              <div className={styles.sectionBody}>
+                <div className={styles.pfpWrapper}>
+                  <Image src={user.photoURL} alt={""} fill={true} />
+                </div>
+                <p className={styles.nameWrapper}>Name: {user.displayName}</p>
+                <p className={styles.uidWrapper}>UID: {user.uid}</p>
+              </div>
+            </section>
+            <section className={styles.section}>
+              <div className={styles.sectionHeadingWrapper}>
+                <h3 className={styles.sectionHeading}>Personal results: </h3>
+              </div>
+              <div className={styles.sectionBody}>
+                <ul className={styles.resultsList}>
+                  {userRepsArray ? (
+                    userRepsArray.map((sport: any) => {
                       return (
-                        <li key={sportSubtype[0]}>
-                          {sportSubtype[0]}: {sportSubtype[1]}
+                        <li key={sport[0]} className={styles.resultSection}>
+                          <h3>{sport[0]}</h3>
+                          <div className={styles.resultListWrapper}>
+                            <ul className={styles.resultList}>
+                              {Object.entries(sport[1]).map(
+                                (sportSubtype: any) => {
+                                  return (
+                                    <li
+                                      key={sportSubtype[0]}
+                                      className={styles.resultItem}
+                                    >
+                                      {sportSubtype[0]}: {sportSubtype[1]}
+                                    </li>
+                                  )
+                                }
+                              )}
+                            </ul>
+                          </div>
                         </li>
                       )
-                    })}
-                  </ul>
-                </li>
-              )
-            })
-          : "Loading..."}
-      </ul>
-    </div>
+                    })
+                  ) : (
+                    <Loader />
+                  )}
+                </ul>
+              </div>
+            </section>
+            <section className={styles.section}>
+              <div className={styles.sectionHeadingWrapper}>
+                <h3 className={styles.sectionHeading}>Friends: </h3>
+              </div>
+              <div className={styles.sectionBody}>
+                <span>In Development...</span>
+              </div>
+            </section>
+          </div>
+        </div>
+      ) : (
+        <Unauthorized />
+      )}
+    </>
   )
 }
-
-// const arrayOfMaxReps = Object.entries(item.MaxReps)
-
-//             console.log(arrayOfMaxReps)
-
-//             return (
-//               <li key={index}>
-//                 <ul>
-//                   {arrayOfMaxReps.map((sport: any) => {
-//                     return <li key={sport[0]}>{sport[0]}</li>
-//                   })}
-//                 </ul>
-//               </li>
