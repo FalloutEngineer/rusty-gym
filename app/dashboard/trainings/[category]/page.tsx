5fa35f2ce7abf525a305ab31c0f0934b398e0661
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-07-04 13:44:24 +0300

    removed unused code

--- app/dashboard/trainings/[category]/page.tsx
+++ app/dashboard/trainings/[category]/page.tsx
@@ -1,14 +1,12 @@
 "use client"
 import React, { useEffect, useState } from "react"
 
-import { GetStaticPaths } from "next"
-
 import Grid from "@/app/components/Dashboard/Grid"
 import MenuButton from "@/app/components/Dashboard/MenuButton"
 
 import styles from "../../dashboard.module.css"
 import { usePathname, useRouter } from "next/navigation"
-import { CategoryType } from "@/app/types"
+
 import {
   getAllTrainingsInCategory,
   getCategories,
@@ -18,8 +16,6 @@ export default function Trainings() {
   const router = useRouter()
 
   const category = usePathname().split("/").pop()
-
-  const [categories, setCategories] = useState<Array<String>>()
 
   const [trainings, setTrainings] = useState<Array<any>>()
 
@@ -31,9 +27,7 @@ export default function Trainings() {
         const categoriesStrings = categoriesRaw.map((category) => category.id)
 
         if (category) {
-          if (categoriesStrings.includes(category)) {
-            setCategories(categoriesStrings)
-          } else {
+          if (!categoriesStrings.includes(category)) {
             router.replace("/404")
           }
         } else {
@@ -50,7 +44,6 @@ export default function Trainings() {
 
         if (trainings) {
           setTrainings(trainings)
-          console.log(trainings)
         }
       }
     }
@@ -65,7 +58,6 @@ export default function Trainings() {
       <Grid>
         {trainings
           ? trainings.map((training) => {
-              console.log(category)
               return (
                 <MenuButton
                   key={training.id}
